e7796d915fab550acb263c888324b34f90f2c900
Louis | Diox <45403791+LouisHyt@users.noreply.github.com>
2024-12-23 14:01:36 +0100

    feat: Added Filter inputs

--- php/view/forum/feed.php
+++ php/view/forum/feed.php
@@ -18,25 +18,65 @@
                 Create a topic
             </button>
         </div>
-    
+
+        
         <aside class="sidebar">
-            <a class="menu-item <?= !$filter ? "active" : "" ?>" href="?ctrl=forum&action=index">
-                <i class="fas fa-list-ul"></i>
-                <span>My topics</span>
-            </a>
-            <a class="menu-item <?= $filter === "responses" ? "active" : "" ?>" href="?ctrl=forum&action=index&filter=responses">
-                <i class="fas fa-reply"></i>
-                <span>My responses</span>
-            </a>
-            <a class="menu-item <?= $filter === "opened" ? "active" : "" ?>" href="?ctrl=forum&action=index&filter=opened">
-                <i class="fas fa-lock-open"></i>
-                <span>Open topics</span>
-            </a>
-            <a class="menu-item <?= $filter === "locked" ? "active" : "" ?>" href="?ctrl=forum&action=index&filter=locked">
-                <i class="fas fa-lock"></i>
-                <span>Closed topics</span>
-            </a>
+            <div class="section">
+                <a class="menu-item <?= !$filter ? "active" : "" ?>" href="?ctrl=forum&action=index">
+                    <i class="fas fa-list-ul"></i>
+                    <span>My topics</span>
+                </a>
+                <a class="menu-item <?= $filter === "responses" ? "active" : "" ?>" href="?ctrl=forum&action=index&filter=responses">
+                    <i class="fas fa-reply"></i>
+                    <span>My responses</span>
+                </a>
+                <a class="menu-item <?= $filter === "opened" ? "active" : "" ?>" href="?ctrl=forum&action=index&filter=opened">
+                    <i class="fas fa-lock-open"></i>
+                    <span>Open topics</span>
+                </a>
+                <a class="menu-item <?= $filter === "locked" ? "active" : "" ?>" href="?ctrl=forum&action=index&filter=locked">
+                    <i class="fas fa-lock"></i>
+                    <span>Closed topics</span>
+                </a>
+            </div>
+            <div class="section">
+                <p>Search by category</p>
+                <select name="" id="">
+                    <option value="web">
+                        <a href="?">Web</a>
+                    </option>
+                    <option value="web">
+                        <a href="?">Mobile</a>
+                    </option>
+                    <option value="web">
+                        <a href="?">Design</a>
+                    </option>
+                </select>
+            </div>
+            <div class="section">
+                <p>Search by tags</p>
+                <form action="">
+                    <div class="tag-item">
+                        <label for="php">php</label>
+                        <input type="checkbox" name="php" id="php">
+                    </div>
+                    <div class="tag-item">
+                        <label for="sql">sql</label>
+                        <input type="checkbox" name="sql" id="sql">
+                    </div>
+                    <div class="tag-item">
+                        <label for="javascript">Javascript</label>
+                        <input type="checkbox" name="javascript" id="javascript">
+                    </div>
+                    <div class="tag-item">
+                        <label for="ai">AI</label>
+                        <input type="checkbox" name="ai" id="ai">
+                    </div>
+                    
+                </form>
+            </div>
         </aside>
+        
     
         <section class="main-content">
             <div class="topic-card">
@@ -46,239 +86,9 @@
                </div>
                <p class="title">How can i increment this value ?</p>
                <div class="footer">
-                    <div class="categories">
-                        <span class="category">PHP</span>
-                        <span class="category">SQL</span>
-                    </div>
-                    <div class="infos">
-                        <div class="status">
-                            <i class="fas fa-lock-open"></i>
-                            <span class="count">Open</span>
-                        </div>
-                        <div class="comments">
-                            <i class="fa-solid fa-comment-dots"></i>
-                            <span class="count">0</span>
-                        </div>
-                    </div>
-               </div>
-            </div>
-            <div class="topic-card">
-               <div class="header">
-                    <span class="author">LouisHyt</span>
-                    <span class="posted-time">15 min ago</span>
-               </div>
-               <p class="title">How can i increment this value ?</p>
-               <div class="footer">
-                    <div class="categories">
-                        <span class="category">PHP</span>
-                        <span class="category">SQL</span>
-                    </div>
-                    <div class="infos">
-                        <div class="status">
-                            <i class="fas fa-lock-open"></i>
-                            <span class="count">Open</span>
-                        </div>
-                        <div class="comments">
-                            <i class="fa-solid fa-comment-dots"></i>
-                            <span class="count">0</span>
-                        </div>
-                    </div>
-               </div>
-            </div>
-            <div class="topic-card">
-               <div class="header">
-                    <span class="author">LouisHyt</span>
-                    <span class="posted-time">15 min ago</span>
-               </div>
-               <p class="title">How can i increment this value ?</p>
-               <div class="footer">
-                    <div class="categories">
-                        <span class="category">PHP</span>
-                        <span class="category">SQL</span>
-                    </div>
-                    <div class="infos">
-                        <div class="status">
-                            <i class="fas fa-lock-open"></i>
-                            <span class="count">Open</span>
-                        </div>
-                        <div class="comments">
-                            <i class="fa-solid fa-comment-dots"></i>
-                            <span class="count">0</span>
-                        </div>
-                    </div>
-               </div>
-            </div>
-            <div class="topic-card">
-               <div class="header">
-                    <span class="author">LouisHyt</span>
-                    <span class="posted-time">15 min ago</span>
-               </div>
-               <p class="title">How can i increment this value ?</p>
-               <div class="footer">
-                    <div class="categories">
-                        <span class="category">PHP</span>
-                        <span class="category">SQL</span>
-                    </div>
-                    <div class="infos">
-                        <div class="status">
-                            <i class="fas fa-lock-open"></i>
-                            <span class="count">Open</span>
-                        </div>
-                        <div class="comments">
-                            <i class="fa-solid fa-comment-dots"></i>
-                            <span class="count">0</span>
-                        </div>
-                    </div>
-               </div>
-            </div>
-            <div class="topic-card">
-               <div class="header">
-                    <span class="author">LouisHyt</span>
-                    <span class="posted-time">15 min ago</span>
-               </div>
-               <p class="title">How can i increment this value ?</p>
-               <div class="footer">
-                    <div class="categories">
-                        <span class="category">PHP</span>
-                        <span class="category">SQL</span>
-                    </div>
-                    <div class="infos">
-                        <div class="status">
-                            <i class="fas fa-lock-open"></i>
-                            <span class="count">Open</span>
-                        </div>
-                        <div class="comments">
-                            <i class="fa-solid fa-comment-dots"></i>
-                            <span class="count">0</span>
-                        </div>
-                    </div>
-               </div>
-            </div>
-            <div class="topic-card">
-               <div class="header">
-                    <span class="author">LouisHyt</span>
-                    <span class="posted-time">15 min ago</span>
-               </div>
-               <p class="title">How can i increment this value ?</p>
-               <div class="footer">
-                    <div class="categories">
-                        <span class="category">PHP</span>
-                        <span class="category">SQL</span>
-                    </div>
-                    <div class="infos">
-                        <div class="status">
-                            <i class="fas fa-lock-open"></i>
-                            <span class="count">Open</span>
-                        </div>
-                        <div class="comments">
-                            <i class="fa-solid fa-comment-dots"></i>
-                            <span class="count">0</span>
-                        </div>
-                    </div>
-               </div>
-            </div>
-            <div class="topic-card">
-               <div class="header">
-                    <span class="author">LouisHyt</span>
-                    <span class="posted-time">15 min ago</span>
-               </div>
-               <p class="title">How can i increment this value ?</p>
-               <div class="footer">
-                    <div class="categories">
-                        <span class="category">PHP</span>
-                        <span class="category">SQL</span>
-                    </div>
-                    <div class="infos">
-                        <div class="status">
-                            <i class="fas fa-lock-open"></i>
-                            <span class="count">Open</span>
-                        </div>
-                        <div class="comments">
-                            <i class="fa-solid fa-comment-dots"></i>
-                            <span class="count">0</span>
-                        </div>
-                    </div>
-               </div>
-            </div>
-            <div class="topic-card">
-               <div class="header">
-                    <span class="author">LouisHyt</span>
-                    <span class="posted-time">15 min ago</span>
-               </div>
-               <p class="title">How can i increment this value ?</p>
-               <div class="footer">
-                    <div class="categories">
-                        <span class="category">PHP</span>
-                        <span class="category">SQL</span>
-                    </div>
-                    <div class="infos">
-                        <div class="status">
-                            <i class="fas fa-lock-open"></i>
-                            <span class="count">Open</span>
-                        </div>
-                        <div class="comments">
-                            <i class="fa-solid fa-comment-dots"></i>
-                            <span class="count">0</span>
-                        </div>
-                    </div>
-               </div>
-            </div>
-            <div class="topic-card">
-               <div class="header">
-                    <span class="author">LouisHyt</span>
-                    <span class="posted-time">15 min ago</span>
-               </div>
-               <p class="title">How can i increment this value ?</p>
-               <div class="footer">
-                    <div class="categories">
-                        <span class="category">PHP</span>
-                        <span class="category">SQL</span>
-                    </div>
-                    <div class="infos">
-                        <div class="status">
-                            <i class="fas fa-lock-open"></i>
-                            <span class="count">Open</span>
-                        </div>
-                        <div class="comments">
-                            <i class="fa-solid fa-comment-dots"></i>
-                            <span class="count">0</span>
-                        </div>
-                    </div>
-               </div>
-            </div>
-            <div class="topic-card">
-               <div class="header">
-                    <span class="author">LouisHyt</span>
-                    <span class="posted-time">15 min ago</span>
-               </div>
-               <p class="title">How can i increment this value ?</p>
-               <div class="footer">
-                    <div class="categories">
-                        <span class="category">PHP</span>
-                        <span class="category">SQL</span>
-                    </div>
-                    <div class="infos">
-                        <div class="status">
-                            <i class="fas fa-lock-open"></i>
-                            <span class="count">Open</span>
-                        </div>
-                        <div class="comments">
-                            <i class="fa-solid fa-comment-dots"></i>
-                            <span class="count">0</span>
-                        </div>
-                    </div>
-               </div>
-            </div>
-            <div class="topic-card">
-               <div class="header">
-                    <span class="author">LouisHyt</span>
-                    <span class="posted-time">15 min ago</span>
-               </div>
-               <p class="title">How can i increment this value ?</p>
-               <div class="footer">
-                    <div class="categories">
-                        <span class="category">PHP</span>
-                        <span class="category">SQL</span>
+                    <div class="tags">
+                        <span class="tag">PHP</span>
+                        <span class="tag">SQL</span>
                     </div>
                     <div class="infos">
                         <div class="status">
